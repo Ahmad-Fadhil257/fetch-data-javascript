6ef3db1828b5bc875e882c9e8f43a527bf55981f
Ahmad-Fadhil257 <2zi009adf@gmail.com>
2025-05-19 12:58:58 +0700

    tugas

--- data.js
+++ data.js
@@ -1,5 +1,4 @@
-const sumber = "pikachu.json"
-
+const sumber = "https://pokeapi.co/api/v2/pokemon/pikachu/"
 fetch(sumber)
   .then ((data) => data.json())
   .then((result) => console.log(result ?? "Ga ada datanya bro"))
