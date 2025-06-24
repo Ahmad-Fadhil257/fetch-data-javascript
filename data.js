03ad549a20821c2a7021de3a6c4b0256081d2844
Ahmad-Fadhil257 <2zi009adf@gmail.com>
2025-06-24 09:17:58 +0700

    tugas

--- data.js
+++ data.js
@@ -1,4 +1,22 @@
-const sumber = "https://pokeapi.co/api/v2/pokemon/pikachu/"
-fetch(sumber)
-  .then ((data) => data.json())
-  .then((result) => console.log(result ?? "Ga ada datanya bro"))
+async function fetchData(){
+
+  try{
+
+      const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+      if(!response.ok){
+          throw new Error("Could not fetch resource");
+      }
+
+      const data = await response.json();
+      const pokemonSprite = data.sprites.front_default;
+      const imgElement = document.getElementById("pokemonSprite");
+
+      imgElement.src = pokemonSprite;
+      imgElement.style.display = "block";
+  }
+  catch(error){
+      console.error(error);
+  }
+}
